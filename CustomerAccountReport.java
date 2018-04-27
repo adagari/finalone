1c16c8cff8d8ffe67a5553cc8441e2ebdb447f60
M Folmar <folmar.meagan@yahoo.com>
2018-04-27 18:08:45 -0500

    Update CustomerAccountReport.java

--- CustomerAccountReport.java
+++ CustomerAccountReport.java
@@ -1,6 +1,6 @@
 /****************************************************************************************
   *   Title: CustomerAccountReport.java PA4
-  *   Authors: Wesley Eaton, Megan Folmar, Adam Garibay
+  *   Authors: Wesley Eaton, Meagan Folmar, Adam Garibay
   *   Date: 4/26/2018 2226
   *   Code version: 1.0
   * 
@@ -20,170 +20,241 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.ListIterator;
 
-public class CustomerAccountReport {
+public class CustomerAccountReport 
+{
+ /*declaring instance variables*/
+ 
     private static ObjectInputStream customers;
     private static ObjectInputStream securities;
 
-    private static List<Customer> customerList = new ArrayList<Customer>( );
-    private static List<Security> securityList = new ArrayList<Security>( );
+    private static List < Customer > customerList = new ArrayList < Customer > ();
+    private static List < Security > securityList = new ArrayList < Security > ();
 
-    public static void main( String[] args) {
-
-        try {
+    public static void main(String [] args) 
+    {
+     /*calling openFiles, processReport, and closeFiles methods*/
+        try 
+        {
             openFiles();
             processReport();
             closeFiles();
-        }
-        catch( IOException ioErr ) {
-            System.err.println( "Ending process." );
-        }
-        catch( Exception err ) {
-            System.err.printf( "Serious error. Ending process.  %s",
-                               err.getClass( ).getName( ) );
-            err.printStackTrace( );
-        }
+            
+        }//end try statement
+        
+        catch(IOException ioErr) 
+        {
+            System.err.println("Ending process."); //print out message if ioErr exception is thrown
+            
+        }//end catch
+        
+        catch(Exception err) 
+        {
+            System.err.printf("Serious error. Ending process.  %s", err.getClass().getName()); //print out message if errException thrown
+            err.printStackTrace();
+            
+        }//end catch
 
-    }
+    }//end main()
+    
+    /*open two input files--customers and securities--to be read*/
 
-    public static void openFiles( )
-    throws IOException {
+    public static void openFiles() throws IOException 
+    {
+      /*try opening customers and securities files and throw exception if it is unable to open them*/
+        try 
+        {
+            customers = new ObjectInputStream(Files.newInputStream(Paths.get("Customer.ser")));
+            securities = new ObjectInputStream(Files.newInputStream(Paths.get("Security.ser")));
+            
+        }//end try statement 
+                
+        catch (IOException ioException) 
+        {
+            System.err.println("Error opening input file."); //print out message if ioException is thrown
+        
+        }//end catch
+        
+    }//end public static void openFiles
+    
+    /*try to process reports and throws Exception if unable to*/
 
-        try {
-            customers = new ObjectInputStream(Files.newInputStream(Paths.get( "Customer.ser")));
-            securities = new ObjectInputStream(Files.newInputStream(Paths.get( "Security.ser")));
-        } catch (IOException ioException) {
-            System.err.println("Error opening input file.");
-        }
-    }
+    public static void processReport() throws Exception 
+    {
+     /*call prepCustomerCollection, prepSecurityCollection, and reportEquityPositions methods*/
+        try 
+        {
+            prepCustomerCollection();
+            prepSecurityCollection();
+            reportEquityPositions();
+            
+        }//end try statement 
+        
+        catch(Exception err) 
+        {
+            System.err.printf("Serious error. Ending proces.  %s", err.getClass().getName()); //print message if err exception thrown
+            err.printStackTrace();
+            
+        }//end catch statement
+        
+    }//end public static void processReport
+    
+    /*close two input files customers and securities*/
 
-    public static void processReport( )
-    throws Exception {
-        try {
-            prepCustomerCollection( );
-            prepSecurityCollection( );
-            reportEquityPositions( );
-        } catch( Exception err ) {
-            System.err.printf( "Serious error. Ending proces.  %s",
-                               err.getClass( ).getName( ) );
-            err.printStackTrace( );
-        }
-    }
+    public static void closeFiles() throws IOException 
+    {
+      /*try closing customers and securities files and throws exception if unable to*/
+        try 
+        {
+            if(customers != null && securities != null) 
+            {
+                customers.close(); //closing customers
+                securities.close(); //closing securities
+            
+            }//end if statement
+        
+        }//end try statement
+        
+        catch (IOException ioException) 
+        {
+            System.err.println("Error closing input files."); //print out message if ioException is thrown
+        
+        }//end catch statement
 
-    public static void closeFiles( )
-    throws IOException {
+    }//end public static void closeFiles
+    
+    /*enter loop to deserialize, store, and sort Customer objects. throw exceptions if unable to*/
 
-        try {
-            if(customers != null && securities != null) {
-                customers.close();
-                securities.close();
-            }
-        }
-        catch (IOException ioException) {
-            System.err.println("Error closing input files.");
-        }
-
-    }
-
-    public static void prepCustomerCollection( )
-    throws ClassNotFoundException,
-        IOException {
-
-        try {
-            while ( true ) {
+    public static void prepCustomerCollection() throws ClassNotFoundException, IOException 
+    {
+        try 
+        {
+            while (true) 
+            {
                 Customer record = (Customer) customers.readObject();
                 customerList.add(record);
 
-            }
-        }
+            }//end while statement
+        
+        }//end try statement
 
-        catch (EOFException endOfFileExeption) {
+        catch (EOFException endOfFileExeption) 
+        {
             Collections.sort(customerList, new Customer_CustNumber());
-        }
+        
+        }//end catch statement
 
-        catch (ClassNotFoundException classNotFoundException) {
+        catch (ClassNotFoundException classNotFoundException) 
+        {
             System.err.println("Invalid object type. Terminating.");
-        } catch (IOException ioException) {
+        
+        }//end catch statement 
+        
+        catch (IOException ioException) 
+        {
             System.err.println("Error reading from file. Terminating.");
-        }
+        
+        }//end catch statement
 
-    }
+    }//end public static void prepCustomerCollection
+    
+    /*enter loop to deserialize, store, and sort Security objects. throw exceptions if unable to*/
 
-    public static void prepSecurityCollection( )
-    throws ClassNotFoundException,
-        IOException {
-
-        try {
-            while ( true ) {
+    public static void prepSecurityCollection() throws ClassNotFoundException, IOException 
+    {
+        try 
+        {
+            while (true) 
+            {
                 Security record = (Security) securities.readObject();
                 securityList.add(record);
-            }
-        }
+                
+            }//end while statement
+        
+        }//end try statement
 
-        catch (EOFException endOfFileExeption) {
+        catch (EOFException endOfFileExeption) 
+        {
             Collections.sort(securityList, new Security_CustNumber());
-        }
-        catch (ClassNotFoundException classNotFoundException) {
+        
+        }//end catch statement
+        
+        catch (ClassNotFoundException classNotFoundException) 
+        {
             System.err.println("Invalid object type. Terminating.");
-        } catch (IOException ioException) {
+        
+        }//end catch statement
+        
+        catch (IOException ioException) 
+        {
             System.err.println("Error reading from file. Terminating.");
-        }
-    }
+        
+        }//end catch statement
+    
+    }//end public static void prepSecurityCollection
+    
+    /*move through Security Collection and have holding spaces for Security and String objects*/
 
-    public static void reportEquityPositions( ) {
-
-        Iterator<Security> securityIterator = securityList.iterator();
+    public static void reportEquityPositions() 
+    {
+        Iterator < Security > securityIterator = securityList.iterator();
         Security equity = (Security)securityIterator.next();
         String holdEquity = equity.getCustNumber();
-        findCustomer( holdEquity );
+        findCustomer(holdEquity);
 
+        try 
+        {
+            while(true) 
+            {
+                while(equity.getCustNumber().matches(holdEquity)) 
+                {
+                    System.out.printf("      %-10s %-5s carries a cost basis of %15s%n", equity.getClass().getName(),
+                                       equity.getSymbol(), String.format("$%,.2f", equity.calcCost()));
 
-        try {
-            while( true ) {
-                while( equity.getCustNumber( ).matches( holdEquity ) ) {
+                    equity = securityIterator.next();
+                
+                }//end while statement
 
-                    System.out.printf( "      %-10s %-5s carries a cost basis of %15s%n",
-                                       equity.getClass( ).getName( ),
-                                       equity.getSymbol( ),
-                                       String.format( "$%,.2f", equity.calcCost( ) ) );
+                System.out.printf("=========================================================================%n%n");
+                holdEquity = equity.getCustNumber();
+                findCustomer(holdEquity);
+                
+            }//end while statement
+        
+        }//end try statement
 
-                    equity = securityIterator.next( );
-                }
+        catch(NoSuchElementException endOfCollection) 
+        {
+            System.out.printf("%n%n\t\t ***** END OF EQUITY REPORT *****%n"); 
 
-                System.out.printf( "=========================================================================%n%n" );
-                holdEquity = equity.getCustNumber( );
-                findCustomer( holdEquity );
-            }
-        }
+        }//end catch statement
 
-        catch( NoSuchElementException endOfCollection ) {
+    }//end public static void reportEquityPositions
+    
+    /*look through Customer collection for specified object, retrieve it, and name it*/
 
-            System.out.printf( "%n%n\t\t ***** END OF EQUITY REPORT *****%n" );
-
-        }
-
-    }
-
-    public static void findCustomer( String findCust ) {
+    public static void findCustomer(String findCust) 
+    {
         Customer target;
         Customer found;
 
-        try {
-
-            target = new Customer( findCust,
-                                   "000000000" );
-
+        try 
+        {
+            target = new Customer(findCust, "000000000");
 
             int result = Collections.binarySearch(customerList, target, new Customer_CustNumber());
             found = customerList.get(result);
 
-            System.out.printf( "%n%-20s %-20s Customer %s, TIN %s%n",
-                               found.getFirst( ),
-                               found.getLast( ),
-                               found.getCustNumber( ),
-                               found.getTin( ) );
+            System.out.printf("%n%-20s %-20s Customer %s, TIN %s%n", found.getFirst(), found.getLast(),
+                               found.getCustNumber(), found.getTin());
 
-        } catch(CustomerException err) {
-            System.err.printf( "Error searching Customer List%n" );
-        }
-    } 
-}
+        }//end try statement 
+        
+        catch(CustomerException err) 
+        {
+            System.err.printf("Error searching Customer List%n");
+        
+        }//end catch statement
+        
+    }//end public static void findCustomer 
+
+}//end public class CustomerAccountReport
